b5d577682046ca4b0468e5c734d4583b901858e4
Felipe Apablaza <fapablaza@outlier.cl>
2016-05-29 23:50:41 -0400

    Refactorizacion de clase LookAtCamera en su propio archivo

--- src/spaceinv/SpaceInvApp.cpp
+++ src/spaceinv/SpaceInvApp.cpp
@@ -19,33 +19,7 @@
 #include "TextureLoader.hpp"
 #include "MeshLoader.hpp"
 #include "PhongPipeline.hpp"
-
-class LookAtCamera : public xe::sg::Camera {
-public:
-	virtual ~LookAtCamera() {}
-
-	virtual xe::Matrix4f computeView() const override {
-		return xe::lookat(position, lookat, up);
-	}
-
-	virtual xe::Matrix4f computeProj() const override {
-		return xe::perspective(fov, aspect, znear, zfar);
-	}
-
-	virtual xe::Rectf getViewport() const override {
-		return xe::Rectf();
-	}
-
-public:
-	xe::Vector3f position = {0.0f, 0.0f, 0.0f};
-	xe::Vector3f lookat = {0.0f, 0.0f, 0.0f};
-	xe::Vector3f up = {0.0f, 0.0f, 0.0f};
-
-	float fov = xe::rad(60.0f);
-	float aspect = 4.0f/3.0f;
-	float znear = 0.0001f;;
-	float zfar = 1000.0f;
-};
+#include "LookAtCamera.hpp"
 
 class SpaceInvApp {
 public:
