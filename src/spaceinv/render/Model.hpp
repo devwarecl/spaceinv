1cf9480fd39c826dca478087e44e44fbf75663aa
Felipe Apablaza <ing.apablaza@gmail.com>
2016-08-15 16:30:43 -0300

    Added Missing include files

---
+++ src/spaceinv/render/Model.hpp
@@ -0,0 +1,39 @@
+
+#pragma once
+
+#ifndef __spaceinv_render_model_hpp__
+#define __spaceinv_render_model_hpp__
+
+#include "xe/Box.hpp"
+#include "xe/gfx/Mesh.hpp"
+
+class SubModel {
+public:
+    SubModel(xe::gfx::Mesh *mesh, const std::size_t coordIndex);
+    
+    size_t getVertexCount() const;
+    
+    Boxf getBox() const;
+    
+private:
+    std::size_t m_coordIndex = 0;
+    xe::gfx::Mesh *m_mesh = nullptr;
+};
+
+class Model {
+public:
+    virtual ~Model() {}
+
+    virtual ModelFrame* getFrame(const std::size_t frameIndex) = 0;
+    
+    virtual const ModelFrame* getFrame(const std::size_t frameIndex) const = 0;
+    
+    virtual std::size_t getFrameIndex() const = 0;
+    
+    virtual void setFrameIndex(const std::size_t frameIndex) = 0;
+    
+private:
+    
+};
+
+#endif
---
+++ src/xe/Box.hpp
@@ -0,0 +1,12 @@
+
+#ifndef __xe_box_hpp__
+#define __xe_box_hpp__
+
+#include "Boundary.hpp"
+
+namespace xe {
+    typedef Boundary<float, 3> Boxf;
+    typedef Boundary<int, 3> Boxi;
+}
+
+#endif
